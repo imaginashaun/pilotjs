8ec3b4f879d2eded8db1083e7911912e88a91597
imaginashaun <idevelop4mobile@gmail.com>
2022-12-27 15:46:09 +0200

    Minor edits

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,22 @@ var express = require('express');
 var fs = require('fs');
 const pdf = require('pdf-parse');
 const EventSource = require('eventsource');
+const he = require('he');
 
 //var {readFilesHandler} = require("../public/javascripts/filecontentReader");
 const formidable = require("formidable");
 
 var router = express.Router();
 
+var stringPrompt = "";
+
 const prompt = `In response to the spike in COVID-19 cases in parts of the world, India is shoring up its defences against the virus. India's Ministry of Health and Family Welfare has noted the spike of COVID cases in China, Brazil, Japan, US and is deploying a series of measures. Officials stress that there is no need for panic, and that India’s COVID numbers are currently at their lowest since the pandemic began, and have been in a steady decline for the past 5 months. China is grappling with a possible COVID-19 comeback. The spike has been noted to take effect since China eased its zero-COVID restrictions last month.`;
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { promptStr: prompt, articleText:"" });
+  stringPrompt = Buffer.from(req.query.ai, 'base64');
+
+
 });
 
 
@@ -56,7 +62,7 @@ router.post('/fileupload', function (req, res, next) {
           body: JSON.stringify({
            // stream:true,
             model: 'text-davinci-003',
-            prompt: "Write a 500 word news article from this text: "+data.text,
+            prompt: stringPrompt !== ""?stringPrompt+": ":"Write a 500 word news article from this text: "+data.text,
             max_tokens: 812,
           }),
 //          https: {rejectUnauthorized: false}
@@ -107,7 +113,9 @@ router.post('/fileupload', function (req, res, next) {
             .then((response) => response.json())
             .then((data) => {
               console.log(data.choices[0].text);
-              res.render('index', {promptStr:prompt, articleText: data.choices[0].text });
+
+              var parsedArticleText = he.decode(data.choices[0].text);
+              res.render('index', {promptStr:prompt, articleText: parsedArticleText });
 
             });
 
